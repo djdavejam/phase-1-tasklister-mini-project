365990ee8b5d4870487073d006c862a2870753f5
David James <davidanthoneyjames@gmail.com>
2025-07-26 09:58:38 -0400

    This is the HTML form lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const task = event.target.elements["new-task-description"].value;
+    buildTodo(task);
+  });
+
+  function buildTodo(task) {
+    const li = document.createElement("li");
+    li.textContent = task;
+    document.getElementById("tasks").appendChild(li);
+  }
 });
